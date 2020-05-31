04184980d5ef772e36c4b5f06676c751f1d239ec
marcinsulikowski <marcinsulikowski@users.noreply.github.com>
2020-05-31 19:27:48 +0000

    deploy: marcinsulikowski/sudoku-assistant@9995d17e81646e2dfce0a05a0ad97fe0149544d8

---
+++ precache-manifest.f7fd7ee5c5ee5bf9b6ad1e15145c894d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3ed5d92c92180b061a777afa5b00bc5",
+    "url": "https://marcinsulikowski.github.io/sudoku-assistant-page/index.html"
+  },
+  {
+    "revision": "3341ed46ffbaa45db7f1",
+    "url": "https://marcinsulikowski.github.io/sudoku-assistant-page/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "da55e3826bbcb58b451d",
+    "url": "https://marcinsulikowski.github.io/sudoku-assistant-page/static/css/main.ff135ee5.chunk.css"
+  },
+  {
+    "revision": "3341ed46ffbaa45db7f1",
+    "url": "https://marcinsulikowski.github.io/sudoku-assistant-page/static/js/2.f325194d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "https://marcinsulikowski.github.io/sudoku-assistant-page/static/js/2.f325194d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da55e3826bbcb58b451d",
+    "url": "https://marcinsulikowski.github.io/sudoku-assistant-page/static/js/main.ef48d6e2.chunk.js"
+  },
+  {
+    "revision": "527a92342a13f9a34d97",
+    "url": "https://marcinsulikowski.github.io/sudoku-assistant-page/static/js/runtime-main.4732db92.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.664a06c40a3084c32601786abf137a72.js"
+  "https://marcinsulikowski.github.io/sudoku-assistant-page/precache-manifest.f7fd7ee5c5ee5bf9b6ad1e15145c894d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://marcinsulikowski.github.io/sudoku-assistant-page/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
